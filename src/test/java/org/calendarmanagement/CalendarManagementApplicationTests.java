11f12512527f1d5495fe7ca8cf7750a8f7c915cc
soyeong <thdud001012@gmail.com>
2026-02-03 17:24:24 +0900

    project settings

---
+++ src/test/java/org/calendarmanagement/CalendarManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.calendarmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
